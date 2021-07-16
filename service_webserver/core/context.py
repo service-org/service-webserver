7aaa5a41baf945ab9fd6aef074ffa4a11b283c2b
李满满 <limanman@corp.netease.com>
2021-07-16 09:20:08 +0800

    :underage:

---
+++ service_webserver/core/context.py
@@ -0,0 +1,19 @@
+#! -*- coding: utf-8 -*-
+#
+# author: forcemain@163.com
+
+from __future__ import annotations
+
+
+import typing as t
+
+def from_headers_to_context(headers: t.Iterable) -> t.Dict:
+    """ 从头部信息提取并生成上下文
+
+    @param headers: 头部信息
+    @return: t.Dict
+    """
+    pass
+
+
+def from_context_
