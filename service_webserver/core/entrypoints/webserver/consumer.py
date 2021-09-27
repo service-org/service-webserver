1e09172f090b07b5dff95a0b7ae6a2b0de7e0897
forcemain <forcemain@163.com>
2021-09-27 12:01:11 +0800

    :underage:

--- service_webserver/core/entrypoints/webserver/consumer.py
+++ service_webserver/core/entrypoints/webserver/consumer.py
@@ -100,6 +100,7 @@ class ReqConsumer(Entrypoint):
         self.include_in_doc = include_in_doc
         self.response_description = response_description
         super(ReqConsumer, self).__init__(**kwargs)
+        self.exec_timing = kwargs.get('exec_timing', 15)
 
     def __repr__(self) -> t.Text:
         name = super(ReqConsumer, self).__repr__()
