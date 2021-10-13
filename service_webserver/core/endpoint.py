b811bffc6e7f79e455e4eef32a9e380338b96934
forcemain <forcemain@163.com>
2021-10-13 14:04:18 +0800

    :underage:

--- service_webserver/core/endpoint.py
+++ service_webserver/core/endpoint.py
@@ -28,13 +28,13 @@ class Endpoint(BaseEndpoint):
         if not self.entrypoint_wrapper: return super_router_mapping
         class_name = self.__class__.__name__
         for method in DEFAULT_METHODS_THAT_WITH_BODY:
+            method_name = method.lower()
             # 将当前类下的entrypoint注入到对应类方法的entrypoints属性中
-            cls_method = getattr(self.__class__, method.lower(), None)
+            cls_method = getattr(self.__class__, method_name, None)
             if not cls_method: continue
             self.entrypoint_wrapper(methods=[method.upper()], **self.entrypoint_options)(cls_method)
             ins_method = getattr(self, method.lower())
             module_name = ins_method.__module__.rsplit('.', 1)[-1]
-            method_name = method.lower()
             # 最终注入到路由映射表中的应该是方法的点分路径和当前实例的方法
             router_mapping.update({f'{module_name}.{class_name}.{method_name}': ins_method})
         return super_router_mapping | router_mapping
